d549fe2fdb15be1eaa8c5835946c78d19c249010
mbauskar <mbauskar@gmail.com>
2016-06-30 10:38:36 +0530

    [API] Register User API

--- teesta/api/utils.py
+++ teesta/api/utils.py
@@ -1,4 +1,5 @@
 import frappe
+from schema import get_allowed_fields
 
 def get_formatted_doc(doc, fields=[], version="1_0_0"):
 	""" get doc as per schema """
@@ -10,3 +11,23 @@ def get_formatted_doc(doc, fields=[], version="1_0_0"):
 	else:
 		[_doc.pop(field) for field in _doc.keys() if field not in fields]
 		return _doc
+
+def send_mail(action, args, subject):
+	""" send mail """
+	from frappe.utils.user import get_user_fullname
+
+	sender = None
+	template = "templates/email/verify_email.html"
+
+	try:
+		args.update({
+			"fullname": get_user_fullname(args.get("user")) or "Guest"
+		})
+
+		frappe.sendmail(recipients=args.get("email"), sender=sender, subject=subject,
+			message=frappe.get_template(template).render(args))
+
+		return True
+	except Exception, e:
+		# raise Exception("Error while sending mail")
+		return False
